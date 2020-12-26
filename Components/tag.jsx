5e4d53442ea2f50b78a03d7348e50d6cb6287eac
BenSegal855 <bensegal@udel.edu>
2020-12-26 15:32:41 -0500

    feat: everything works

---
+++ Components/tag.jsx
@@ -0,0 +1,10 @@
+const { React } = require('powercord/webpack');
+
+class Tag extends React.PureComponent {
+  render () {
+    return <div className={`${this.props.className}`}>WEBHOOK</div>;
+  }
+}
+
+Tag.cache = {};
+module.exports = Tag;
---
+++ index.js
@@ -0,0 +1,40 @@
+const { Plugin } = require('powercord/entities');
+const { getModule, React } = require('powercord/webpack');
+const { inject, uninject } = require('powercord/injector');
+const { findInReactTree } = require('powercord/util');
+
+const Tag = require('./Components/Tag');
+
+module.exports = class WebTag extends Plugin {
+  startPlugin () {
+    const MessageTimestamp = getModule([ 'MessageTimestamp' ], false);
+    const botTagRegularClasses = getModule([ 'botTagRegular' ], false);
+    const botTagCozyClasses = getModule([ 'botTagCozy' ], false);
+    const remClasses = getModule([ 'rem' ], false);
+
+    inject('webhook-tag', MessageTimestamp, 'default', (args, res) => {
+      const msg = args[0].message;
+
+      if (msg.webhookId !== null && msg.messageReference === null) {
+        args[0].message.author.bot = false;
+
+        const header = findInReactTree(res, e => Array.isArray(e.props?.children) && e.props.children.find(c => c.props?.message));
+        header.props.children.push(React.createElement(
+          'span',
+          {
+            className: `${botTagCozyClasses.botTagCozy} ${botTagRegularClasses.botTagRegular} ${remClasses.rem}`
+          },
+          React.createElement(Tag, {
+            className: botTagRegularClasses.botText
+          })
+        ));
+      }
+
+      return res;
+    });
+  }
+
+  pluginWillUnload () {
+    uninject('webhook-tag');
+  }
+};
